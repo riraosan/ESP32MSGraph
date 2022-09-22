99051b2f395497b085028211f338ec0dafb881e2
riraosan <fiatpanda750@icloud.com>
2022-09-22 20:46:07 +0900

    format

--- src/filter.h
+++ src/filter.h
@@ -22,31 +22,33 @@ Contributors:
 
 constexpr char loginFilter[] = R"(
 {
-  "user_code" : true,
-  "device_code" : true,
+  "user_code"        : true,
+  "device_code"      : true,
   "verification_uri" : true,
-  "expires_in" : true,
-  "interval": true,
-  "message" : true
+  "expires_in"       : true,
+  "interval"         : true,
+  "message"          : true
 }
 )";
 
 constexpr char refleshtokenFilter[] = R"(
 {
-  "token_type" : true,
-  "scope" : true,
-  "expires_in" : true,
+  "token_type"     : true,
+  "scope"          : true,
+  "expires_in"     : true,
   "ext_expires_in" : true,
-  "access_token" : true,
-  "refresh_token" : true,
-  "id_token" : true
+  "access_token"   : true,
+  "refresh_token"  : true,
+  "id_token"       : true
 }
 )";
 
 constexpr char presenceFilter[] = R"(
 {
-  "id": true,
-  "availability": true,
-  "activity": true"
+  "id"           : true,
+  "availability" : true,
+  "activity"     : true"
 }
 )";
+
+// TODO Please add User filter below.
