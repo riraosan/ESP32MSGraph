02224fe90ec1adcf24e11e6fafffdc4fc8321739
riraosan <fiatpanda750@icloud.com>
2022-10-07 12:30:53 +0900

    add WS2812FX LED View

--- example/00_basic/presence/presence.ino
+++ example/00_basic/presence/presence.ino
@@ -21,29 +21,75 @@ Contributors:
 #include <ESP32MSGraph.h>
 using WebServerClass = WebServer;
 #include <Ticker.h>
-
-Ticker _timer;
-bool   flag = false;
-
-void _interval(void) {
-  flag = true;
-}
+#include <WS2812FX.h>
 
 WebServerClass server;
 ESP32MSGraph   msgraph(&server);
+
+WS2812FX     ws2812fx(25, 27, NEO_GRB + NEO_KHZ800);
+TaskHandle_t TaskNeopixel;
+String       _availability;
+
+Ticker _timer;
+bool   flag = false;
 
 // Please add User filter.
 constexpr char presenceFilter[] = R"(
 {
   "id"           : false,
   "availability" : true,
-  "activity"     : false"
+  "activity"     : true"
 }
 )";
 
 StaticJsonDocument<200> _presenceFilter;
 
-String _availability;
+void _interval(void) {
+  flag = true;
+}
+
+void setAnimation(uint8_t brightness, uint16_t speed, uint32_t color, uint8_t mode) {
+  ws2812fx.setBrightness(brightness);
+  ws2812fx.setSpeed(speed);
+  ws2812fx.setColor(color);
+  ws2812fx.setMode(mode);
+}
+
+void setPresenceAnimation(String activity) {
+  if (activity.equals("Available")) {
+    setAnimation(255, 1000, GREEN, FX_MODE_BREATH);
+  }
+  if (activity.equals("Away")) {
+    setAnimation(255, 1000, YELLOW, FX_MODE_BREATH);
+  }
+  if (activity.equals("BeRightBack")) {
+    setAnimation(255, 1000, ORANGE, FX_MODE_BREATH);
+  }
+  if (activity.equals("Busy")) {
+    setAnimation(255, 1000, PURPLE, FX_MODE_BREATH);
+  }
+  if (activity.equals("DoNotDisturb") || activity.equals("UrgentInterruptionsOnly")) {
+    setAnimation(255, 1000, PINK, FX_MODE_BREATH);
+  }
+  if (activity.equals("InACall")) {
+    setAnimation(255, 1000, RED, FX_MODE_BREATH);
+  }
+  if (activity.equals("InAConferenceCall")) {
+    setAnimation(255, 9000, RED, FX_MODE_BREATH);
+  }
+  if (activity.equals("Inactive")) {
+    setAnimation(255, 1000, WHITE, FX_MODE_BREATH);
+  }
+  if (activity.equals("InAMeeting")) {
+    setAnimation(255, 1000, RED, FX_MODE_SCAN);
+  }
+  if (activity.equals("Offline") || activity.equals("OffWork") || activity.equals("OutOfOffice") || activity.equals("PresenceUnknown")) {
+    setAnimation(255, 1000, BLACK, FX_MODE_STATIC);
+  }
+  if (activity.equals("Presenting")) {
+    setAnimation(255, 1000, RED, FX_MODE_COLOR_WIPE);
+  }
+}
 
 bool pollPresence(void) {
   log_d("pollPresence()");
@@ -60,9 +106,10 @@ bool pollPresence(void) {
 
   if (res) {
     // Store presence info
-    _availability = responseDoc["availability"].as<String>();
-    // String activity     = responseDoc["activity"].as<String>();
-    log_i("success to get Presence: %s", _availability.c_str());
+    //_availability = responseDoc["availability"].as<String>();
+    String activity = responseDoc["activity"].as<String>();
+    setPresenceAnimation(activity);
+    log_i("success to get Presence: %s", activity.c_str());
   } else if (responseDoc.containsKey("error")) {
     const char* _error_code = responseDoc["error"]["code"];
     if (strcmp(_error_code, "InvalidAuthenticationToken")) {
@@ -77,14 +124,32 @@ bool pollPresence(void) {
   return res;
 }
 
+void neopixelTask(void* parameter) {
+  for (;;) {
+    ws2812fx.service();
+    delay(1);
+  }
+}
+
 void setup() {
   Serial.begin(115200);
+
   // ATOM Lite WiFi不具合対策
   pinMode(0, OUTPUT);
   digitalWrite(0, LOW);
 
   deserializeJson(_presenceFilter, presenceFilter);
   _timer.attach(30, _interval);
+
+  ws2812fx.init();
+  ws2812fx.setBrightness(128);
+  ws2812fx.setSpeed(1000);
+  ws2812fx.setColor(WHITE);
+  ws2812fx.setMode(FX_MODE_BLINK);
+  ws2812fx.start();
+
+  // Pin neopixel logic to core 0
+  xTaskCreatePinnedToCore(neopixelTask, "Neopixels", 1024, NULL, 1, &TaskNeopixel, 1);
 
   msgraph.begin();
 }
@@ -94,5 +159,6 @@ void loop() {
     flag = false;
     pollPresence();
   }
+
   msgraph.update();
 }
